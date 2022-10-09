4a57bfb4ccfc1aef77f2b3511fde4154640f4eb0
Sara Cagle <sacagle@microsoft.com>
2022-10-08 19:47:49 -0700

    save

--- js/controllers.js
+++ js/controllers.js
@@ -124,7 +124,7 @@ angular.module('PortfolioApp', ['ngRoute', 'ngAnimate'])
 				controller: 'contentController',
 			})
 			.when('/projecta1', {
-				template: '../views/hcde533/a1.html',
+				template: 'views/hcde533/a1.html',
 				controller: 'contentController'
 			})
 			.when('/hcde533/a2', {
