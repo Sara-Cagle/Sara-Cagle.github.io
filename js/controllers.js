9c291dd4ebb0d852ff2049d76131753408c2a2a1
Sara Cagle <sacagle@microsoft.com>
2022-10-09 20:03:30 -0700

    apply linting

--- js/controllers.js
+++ js/controllers.js
@@ -1,158 +1,153 @@
+// eslint-disable-next-line no-undef
 angular.module('PortfolioApp', ['ngRoute', 'ngAnimate'])
 
+    /*
+                    * Due to the CSS, these actually are loaded into the page from right to left (backwards), so put the most important links
+                    * at the bottom of this list, so they will appear closest to the left.
+                    */
+    .controller('navController', [function ($route, $routeParams) {
+        this.links = [
+            {
+                linkName: 'Contact',
+                URL: '#/contact',
+            },
+            // {linkName: 'Experience',
+            // URL: '#/experience',
+            // },
+            // {linkName: 'Projects',
+            // URL: '#/projects',
+            // },
+            {
+                linkName: 'About',
+                URL: '#/about',
+            },
+            {
+                linkName: 'HCDE 533',
+                URL: '#/hcde533',
+            },
+        ];
+    }])
 
-	/*
-	* Due to the CSS, these actually are loaded into the page from right to left (backwards), so put the most important links
-	* at the bottom of this list, so they will appear closest to the left.
-	*/
-	.controller('navController', [function ($route, $routeParams) {
-		this.links = [
-			{
-				linkName: 'Contact',
-				URL: '#/contact'
-			},
-			// {linkName: 'Experience',
-			// URL: '#/experience',
-			// },
-			// {linkName: 'Projects',
-			// URL: '#/projects',
-			// },
-			{
-				linkName: 'About',
-				URL: '#/about',
-			},
-			{
-				linkName: 'HCDE 533',
-				URL: '#/hcde533'
-			}
-		]
-	}])
+    .controller('contentController', ['$scope', function ($scope, $route, $routeParams) {
+        /*
+            * For toggling the WeChat QR code modal.
+            */
+        $scope.showModal = false;
+        $scope.openModal = function () {
+            $scope.showModal = !$scope.showModal;
+        };
+    }])
 
-	.controller('contentController', ['$scope', function ($scope, $route, $routeParams) {
+    .controller('hcdeProjectController', ['$scope', function ($scope, $route, $routeParams) {
+        /* HCDE 533 projects */
+        this.projects = [
+            {
+                id: 'a1',
+                title: 'A1 - Modeling & Laser Cutting - Cardboard Centipede',
+                description: 'Two pieces designed to be interlocked and repeatable to create a cardboard model of a centipede. Pieces were modeled in Rhino 7 and then laser cut on an Epilog Laser.',
+                URL: '#/hcde533/a1',
+            },
+            {
+                id: 'a2',
+                title: 'A2 - 3D Printing',
+                description: 'TBD',
+                URL: '#/hcde533/a2',
+            },
+            {
+                id: 'a3',
+                title: 'A3 - Parametric Modeling with Grasshopper',
+                description: 'TBD',
+                URL: '#/hcde533/a3',
+            },
+            {
+                id: 'a4',
+                title: 'A4 - Mesh & Lamps Part 1',
+                description: 'TBD',
+                URL: '#/hcde533/a4',
+            },
+            {
+                id: 'a5',
+                title: 'A5 - Molding & Casting Part 1, Lamps Part 2',
+                description: 'TBD',
+                URL: '#/hcde533/a5',
+            },
+            {
+                id: 'a6',
+                title: 'A6 - Molding & Casting Part 2',
+                description: 'TBD',
+                URL: '#/hcde533/a6',
+            },
+            {
+                id: 'a7',
+                title: 'A7 - Final Project Proposal',
+                description: 'TBD',
+                URL: '#/hcde533/a7',
+            },
+            {
+                id: 'a8',
+                title: 'A8 - Final Project',
+                description: 'TBD',
+                URL: '#/hcde533/a8',
+            },
+        ];
+    }])
 
-		/*
-		* For toggling the WeChat QR code modal.
-		*/
-		$scope.showModal = false;
-		$scope.openModal = function () {
-			$scope.showModal = !$scope.showModal;
-		};
-
-
-	}])
-
-	.controller('hcdeProjectController', ['$scope', function ($scope, $route, $routeParams) {
-		/* HCDE 533 projects */
-		this.projects = [
-			{
-				id: 'a1',
-				title: 'A1 - Modeling & Laser Cutting - Cardboard Centipede',
-				description: 'Two pieces designed to be interlocked and repeatable to create a cardboard model of a centipede. Pieces were modeled in Rhino 7 and then laser cut on an Epilog Laser.',
-				URL: '#/hcde533/a1'
-			},
-			{
-				id: 'a2',
-				title: 'A2 - 3D Printing',
-				description: 'TBD',
-				URL: '#/hcde533/a2'
-			},
-			{
-				id: 'a3',
-				title: 'A3 - Parametric Modeling with Grasshopper',
-				description: 'TBD',
-				URL: '#/hcde533/a3'
-			},
-			{
-				id: 'a4',
-				title: 'A4 - Mesh & Lamps Part 1',
-				description: 'TBD',
-				URL: '#/hcde533/a4'
-			},
-			{
-				id: 'a5',
-				title: 'A5 - Molding & Casting Part 1, Lamps Part 2',
-				description: 'TBD',
-				URL: '#/hcde533/a5'
-			},
-			{
-				id: 'a6',
-				title: 'A6 - Molding & Casting Part 2',
-				description: 'TBD',
-				URL: '#/hcde533/a6'
-			},
-			{
-				id: 'a7',
-				title: 'A7 - Final Project Proposal',
-				description: 'TBD',
-				URL: '#/hcde533/a7'
-			},
-			{
-				id: 'a8',
-				title: 'A8 - Final Project',
-				description: 'TBD',
-				URL: '#/hcde533/a8'
-			},
-		]
-
-
-	}])
-
-	.config(function ($routeProvider) { //routing needs to be on a server in order to run
-		$routeProvider
-			.when('/', {
-				templateUrl: 'views/hcde533.html',
-			})
-			.when('/about', {
-				templateUrl: 'views/about.html',
-				controller: 'contentController',
-			})
-			.when('/experience', {
-				templateUrl: 'views/experience.html',
-				controller: 'contentController'
-			})
-			.when('/projects', {
-				templateUrl: 'views/project.html',
-				controller: 'contentController'
-			})
-			.when('/contact', {
-				templateUrl: 'views/contact.html',
-				controller: 'contentController',
-			})
-			/* For HCDE 533 routes, not dynamic since not standardized from a DB */
-			.when('/hcde533', {
-				templateUrl: 'views/hcde533.html',
-				controller: 'contentController',
-			})
-			.when('/hcde533/a1', {
-				templateUrl: 'views/hcde533/a1.html',
-				controller: 'contentController'
-			})
-			.when('/hcde533/a2', {
-				templateUrl: 'views/hcde533/a2.html',
-				controller: 'contentController'
-			})
-			.when('/hcde533/a3', {
-				templateUrl: 'views/hcde533/a3.html',
-				controller: 'contentController'
-			})
-			.when('/hcde533/a4', {
-				templateUrl: 'views/hcde533/a4.html',
-				controller: 'contentController'
-			})
-			.when('/hcde533/a5', {
-				templateUrl: 'views/hcde533/a5.html',
-				controller: 'contentController'
-			})
-			.when('/hcde533/a6', {
-				templateUrl: 'views/hcde533/a6.html',
-				controller: 'contentController'
-			})
-			.when('/hcde533/a7', {
-				templateUrl: 'views/hcde533/a7.html',
-				controller: 'contentController'
-			})
-			.when('/hcde533/a8', {
-				templateUrl: 'views/hcde533/a8.html',
-				controller: 'contentController'
-			});
-	})
+    .config(($routeProvider) => { // routing needs to be on a server in order to run
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/hcde533.html',
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'contentController',
+            })
+            .when('/experience', {
+                templateUrl: 'views/experience.html',
+                controller: 'contentController',
+            })
+            .when('/projects', {
+                templateUrl: 'views/project.html',
+                controller: 'contentController',
+            })
+            .when('/contact', {
+                templateUrl: 'views/contact.html',
+                controller: 'contentController',
+            })
+            /* For HCDE 533 routes, not dynamic since not standardized from a DB */
+            .when('/hcde533', {
+                templateUrl: 'views/hcde533.html',
+                controller: 'contentController',
+            })
+            .when('/hcde533/a1', {
+                templateUrl: 'views/hcde533/a1.html',
+                controller: 'contentController',
+            })
+            .when('/hcde533/a2', {
+                templateUrl: 'views/hcde533/a2.html',
+                controller: 'contentController',
+            })
+            .when('/hcde533/a3', {
+                templateUrl: 'views/hcde533/a3.html',
+                controller: 'contentController',
+            })
+            .when('/hcde533/a4', {
+                templateUrl: 'views/hcde533/a4.html',
+                controller: 'contentController',
+            })
+            .when('/hcde533/a5', {
+                templateUrl: 'views/hcde533/a5.html',
+                controller: 'contentController',
+            })
+            .when('/hcde533/a6', {
+                templateUrl: 'views/hcde533/a6.html',
+                controller: 'contentController',
+            })
+            .when('/hcde533/a7', {
+                templateUrl: 'views/hcde533/a7.html',
+                controller: 'contentController',
+            })
+            .when('/hcde533/a8', {
+                templateUrl: 'views/hcde533/a8.html',
+                controller: 'contentController',
+            });
+    });
--- js/script.js
+++ js/script.js
@@ -1,64 +1,54 @@
-
-var screenWidth = window.innerWidth;
-var verticalPosition = window.pageYOffset | document.body.scrollTop;
-
+let screenWidth = window.innerWidth;
+let verticalPosition = window.pageYOffset | document.body.scrollTop;
 
 /**
 * Event Listener for resizing the window to help ensure the menu is toggled correctly for the window size.
 */
-window.addEventListener('resize', function(event){
-	screenWidth = window.innerWidth;
-	if(document.getElementById('menu').style.display != "inline"){
-		if(screenWidth>=768){
-			document.getElementById('menu').style.display = "inline";
-		}
-	}
-	else{
-		if(screenWidth<768){
-			document.getElementById('menu').style.display = "none";
-		}
-	}
+window.addEventListener('resize', (event) => {
+    screenWidth = window.innerWidth;
+    if (document.getElementById('menu').style.display !== 'inline') {
+        if (screenWidth >= 768) {
+            document.getElementById('menu').style.display = 'inline';
+        }
+    } else if (screenWidth < 768) {
+        document.getElementById('menu').style.display = 'none';
+    }
 });
-
 
 /**
 * Changes the transparancy of the nav menu when the page is scrolled up or down.
 * Has a few mini bugs, but gets the job done for the most part.
 */
-navTransparencyChange = function(){
-	screenWidth = window.innerWidth;
-	var element = document.getElementById('newnavbar');
-	if(screenWidth >=768){
-		verticalPosition = window.pageYOffset | document.body.scrollTop;
-		if(verticalPosition >=50){
-			element.style.opacity = "1";
-			element.style.filter  = 'alpha(opacity=100)';
-			element.style.borderBottom= "1px solid white";
-		}
-		else{
-			element.style.opacity = "0.6";
-			element.style.filter  = 'alpha(opacity=60)';
-			element.style.borderBottom= "none";
-		}
-	}
-	else{
-		element.style.opacity = "1";
-		element.style.filter  = 'alpha(opacity=100)';
-		element.style.borderBottom= "1px solid white";
-	}
-}
-
+function navTransparencyChange() {
+    screenWidth = window.innerWidth;
+    const element = document.getElementById('newnavbar');
+    if (screenWidth >= 768) {
+        verticalPosition = window.pageYOffset | document.body.scrollTop;
+        if (verticalPosition >= 50) {
+            element.style.opacity = '1';
+            element.style.filter = 'alpha(opacity=100)';
+            element.style.borderBottom = '1px solid white';
+        } else {
+            element.style.opacity = '0.6';
+            element.style.filter = 'alpha(opacity=60)';
+            element.style.borderBottom = 'none';
+        }
+    } else {
+        element.style.opacity = '1';
+        element.style.filter = 'alpha(opacity=100)';
+        element.style.borderBottom = '1px solid white';
+    }
+};
 
 /**
 * Toggles the menu on the click of the hamburger 'more' icon. Works via CSS/
 */
-document.getElementById('show-menu').onclick = function(){
-	if(document.getElementById('menu').style.display === "block"){
-		document.getElementById('menu').style.display = "none";
-	}
-	else{
-		document.getElementById('menu').style.display = "block";
-	}
+document.getElementById('show-menu').onclick = () => {
+    if (document.getElementById('menu').style.display === 'block') {
+        document.getElementById('menu').style.display = 'none';
+    } else {
+        document.getElementById('menu').style.display = 'block';
+    }
 };
 
 /**
@@ -66,13 +56,13 @@ document.getElementById('show-menu').onclick = function(){
 * If in desktop mode, nothing should happen.
 * This works alongside the above function that will toggle the menu closed if you click on the hamburger button.
 */
-document.getElementById('menu').onclick = function(){
-	screenWidth = window.innerWidth;
-	if(screenWidth<768){
-		if(document.getElementById('menu').style.display === "block"){
-			document.getElementById('menu').style.display = "none";
-		}
-	}
+document.getElementById('menu').onclick = () => {
+    screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+        if (document.getElementById('menu').style.display === 'block') {
+            document.getElementById('menu').style.display = 'none';
+        }
+    }
 };
 
 /*
@@ -80,14 +70,13 @@ document.getElementById('menu').onclick = function(){
 * Used when any of the buttons are clicked.
 * Smooth scrolling only works in FireFox, unfortunately, otherwise it just jumps.
 */
-/*var links = document.getElementsByClassName('clickForScroll'){
-	for(var i=0; i<links.length; i++){
-		links[i].onclick=function(){
-			document.getElementById('scrollTo').scrollIntoView({behavior: "smooth"});
-		};
-	}
-}*/
-/*document.getElementById('menu').onclick = function(){
-	document.getElementById('scrollTo').scrollIntoView({behavior: "smooth"});
-};*/
-
+/* var links = document.getElementsByClassName('clickForScroll'){
+    for(var i=0; i<links.length; i++){
+        links[i].onclick=function(){
+            document.getElementById('scrollTo').scrollIntoView({behavior: "smooth"});
+        };
+    }
+} */
+/* document.getElementById('menu').onclick = function(){
+    document.getElementById('scrollTo').scrollIntoView({behavior: "smooth"});
+}; */
